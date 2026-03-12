916e31dd4f8681efe05929d4c1d7083bb53d8269
Oklevet <nikstar1235@gmail.com>
2026-03-12 14:15:17 +0300

    4. Утверждения с исключениями [#504886]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkParseEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::parse)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("is empty");
+    }
+
+    @Test
+    void checkDoesNotContainKey() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "Name1";
+        String[] names = {name};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain the symbol")
+                .hasMessageContaining(name);
+    }
+
+    @Test
+    void checkStartsWithEquals() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "=Name1";
+        String[] names = {name};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain a key")
+                .hasMessageContaining(name);
+    }
+
+    @Test
+    void checkContainsValue() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "Name1=";
+        String[] names = {name};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain a value")
+                .hasMessageContaining(name);
+    }
+
+
+}
