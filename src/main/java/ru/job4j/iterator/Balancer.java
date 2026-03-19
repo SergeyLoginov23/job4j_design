07fdcb72196e14ffa913fae623da6288c93c31e4
SergeyLoginov23 <Professional23@mail.ru>
2026-03-19 19:05:28 +0300

    7. Балансир [#505083]

---
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -0,0 +1,19 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Balancer {
+    public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+       int index = 0;
+       while (source.hasNext()) {
+           if (index < nodes.size()) {
+               nodes.get(index).add(source.next());
+               index++;
+           } else {
+               index = 0;
+           }
+       }
+    }
+}
