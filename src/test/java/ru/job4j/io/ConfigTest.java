30a92b176b596d5ba697e1c53aff0156d88e30f9
SergeyLoginov23 <Professional23@mail.ru>
2026-04-05 20:22:49 +0300

    6. Читаем файл конфигурации [#858]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
+    }
+
+    @Test
+    void whenPairBrokenNames() {
+        String path = "./data/broken_values.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("=======Petr Arsentev=======");
+    }
+
+    @Test
+    void whenNoDataString() {
+        String path = "./data/no_data_string.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isNull();
+    }
+
+    @Test
+    void whenException() {
+        String path = "./data/exception.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenExceptionWithOneLine() {
+        String path = "./data/exception_one_line.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
