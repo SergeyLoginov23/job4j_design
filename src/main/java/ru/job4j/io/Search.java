118bd7dfd6ce312914bf145ec4375f40862bc86a
SergeyLoginov23 <Professional23@mail.ru>
2026-04-11 22:54:36 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,29 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validateInput(args);
+        Path start = Paths.get(args[0]);
+        String searchDetails = args[1];
+        search(start, path -> path.toFile().getName().endsWith(searchDetails)).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validateInput(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("No enough inputs");
+        }
+        Path start = Paths.get(args[0]);
+        String searchDetails = args[1];
+        if (!Files.isDirectory(start) || !Files.exists(start)) {
+            throw new IllegalArgumentException("Root folder is null or not a directory");
+        }
+        if (searchDetails.isEmpty()) {
+            throw new IllegalArgumentException("No filter for search");
+        }
     }
 }
