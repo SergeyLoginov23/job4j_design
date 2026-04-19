ce22369196b99a6bb4de743b3b4e631ce16e487e
SergeyLoginov23 <Professional23@mail.ru>
2026-04-19 19:42:30 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -5,6 +5,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+
+    private static final String MSG = "msg=";
+    private static final String EXIT = "Exit";
+    private static final String HELLO = "Hello";
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -12,16 +17,25 @@ public class EchoServer {
                     try (OutputStream output = socket.getOutputStream();
                          BufferedReader input = new BufferedReader(
                                  new InputStreamReader(socket.getInputStream()))) {
-                        output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                        for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
-                            if (string.contains("msg=Bye")) {
-                                server.close();
-                                break;
-                            }
-                            System.out.println(string);
-                        }
-                        output.flush();
-                    }
+                                 output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                                    for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
+                                        if (string.contains(MSG)) {
+                                            int startIndex = string.indexOf(MSG) + MSG.length();
+                                            string = string.substring(startIndex);
+                                            string = string.substring(0, string.indexOf(" "));
+                                            if (EXIT.equals(string)) {
+                                                server.close();
+                                                break;
+                                            }
+                                            if (HELLO.equals(string)) {
+                                                output.write(HELLO.getBytes());
+                                                continue;
+                                            }
+                                            output.write(string.getBytes());
+                                        }
+                                    }
+                                    output.flush();
+                                }
                 } catch (IOException e) {
                     if (server.isClosed()) {
                         System.out.println("Сервер планово закрыт");
