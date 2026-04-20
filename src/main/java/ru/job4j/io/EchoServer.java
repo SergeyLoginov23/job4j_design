d355092b899977602003f10b7d09cf0ae4aea1c8
SergeyLoginov23 <Professional23@mail.ru>
2026-04-20 20:35:25 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,34 +1,52 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+
+    private static final String MSG = "msg=";
+    private static final String EXIT = "Exit";
+    private static final String HELLO = "Hello";
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 try (Socket socket = server.accept();) {
                     try (OutputStream output = socket.getOutputStream();
                          BufferedReader input = new BufferedReader(
                                  new InputStreamReader(socket.getInputStream()))) {
-                        output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                        String string = input.readLine();
-                        if (string.contains("msg=Bye")) {
-                           server.close();
-                        } else {
-                            System.out.println(string);
-                        }
-                        output.flush();
-                    }
+                                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                                     String string = input.readLine();
+                                     if (string.contains(MSG)) {
+                                         int startIndex = string.indexOf(MSG) + MSG.length();
+                                         string = string.substring(startIndex);
+                                         string = string.substring(0, string.indexOf(" "));
+                                         if (EXIT.equals(string)) {
+                                             server.close();
+                                         } else if (HELLO.equals(string)) {
+                                             output.write(HELLO.getBytes());
+                                         } else {
+                                             output.write(string.getBytes());
+                                         }
+                                     }
+                                     output.flush();
+                                }
                 } catch (IOException e) {
                     if (server.isClosed()) {
-                        System.out.println("Сервер планово закрыт");
+                        LOG.info("Сервер планово закрыт");
                         break;
                     }
-                    e.printStackTrace();
+                    LOG.error("Server error", e);
                 }
             }
+        } catch (IOException e) {
+            LOG.error("Server error", e);
         }
     }
 }
