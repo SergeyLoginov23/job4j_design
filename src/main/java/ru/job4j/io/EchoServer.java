7b7be76e4f4bcca3a2212a30c2a2a26479cb6d85
SergeyLoginov23 <Professional23@mail.ru>
2026-04-20 15:07:41 +0300

    2. Что такое Socket? [#4850 #562614]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -5,11 +5,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-
-    private static final String MSG = "msg=";
-    private static final String EXIT = "Exit";
-    private static final String HELLO = "Hello";
-
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -17,22 +12,15 @@ public class EchoServer {
                     try (OutputStream output = socket.getOutputStream();
                          BufferedReader input = new BufferedReader(
                                  new InputStreamReader(socket.getInputStream()))) {
-                                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                                     String string = input.readLine();
-                                     if (string.contains(MSG)) {
-                                         int startIndex = string.indexOf(MSG) + MSG.length();
-                                         string = string.substring(startIndex);
-                                         string = string.substring(0, string.indexOf(" "));
-                                         if (EXIT.equals(string)) {
-                                             server.close();
-                                         } else if (HELLO.equals(string)) {
-                                             output.write(HELLO.getBytes());
-                                         } else {
-                                             output.write(string.getBytes());
-                                         }
-                                     }
-                                     output.flush();
-                                }
+                        output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        String string = input.readLine();
+                        if (string.contains("msg=Bye")) {
+                           server.close();
+                        } else {
+                            System.out.println(string);
+                        }
+                        output.flush();
+                    }
                 } catch (IOException e) {
                     if (server.isClosed()) {
                         System.out.println("Сервер планово закрыт");
